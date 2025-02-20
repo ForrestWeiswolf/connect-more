92616b555efd3b71727a12ce89e90e4b048984fa
Forrest Wolf <forrest.weiswolf@gmail.com>
2025-02-20 12:57:12 -0500

    Add drop buttons to UI

--- src/app/board/board.component.spec.ts
+++ src/app/board/board.component.spec.ts
@@ -17,24 +17,24 @@ describe('BoardComponent', () => {
     fixture.detectChanges();
   });
 
-  it('has six rows', () => {
+  it('has six rows, plus the row of drop buttons', () => {
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelectorAll('div.row').length).toBe(6);
+    expect(compiled.querySelectorAll<HTMLElement>('div.row').length).toBe(7);
   });
 
   it('has seven cells per row', () => {
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('div.row')?.querySelectorAll('.cell')?.length).toBe(7);
+    expect(compiled.querySelectorAll('div.row')[1]?.querySelectorAll('.cell')?.length).toBe(7);
   });
 
   it('fills the last unfilled cell in a clicked column', fakeAsync(() => {
     const compiled = fixture.nativeElement as HTMLElement;
-    compiled.querySelectorAll('div.row')[0].querySelectorAll<HTMLElement>('.cell')[0]?.click()
+    compiled.querySelectorAll<HTMLElement>('.drop-button')[0]?.click()
 
     tick()
     fixture.detectChanges()
 
-    expect(compiled.querySelectorAll('div.row')[5].querySelectorAll('.cell')[0]?.className).toContain('red');
+    expect(compiled.querySelectorAll('div.row')[6].querySelectorAll('.cell')[0]?.className).toContain('red');
     expect(compiled.querySelectorAll('div.row')[0].querySelectorAll('.cell')[0]?.className).not.toContain('red');
 
     compiled.querySelectorAll('div.row')[0].querySelectorAll<HTMLElement>('div.cell')[0]?.click()
@@ -42,33 +42,32 @@ describe('BoardComponent', () => {
 
   it('alternates colors', fakeAsync(() => {
     const compiled = fixture.nativeElement as HTMLElement;
-    compiled.querySelectorAll('div.row')[0].querySelectorAll<HTMLElement>('.cell')[0]?.click()
-
-    compiled.querySelectorAll('div.row')[0].querySelectorAll<HTMLElement>('div.cell')[0]?.click()
+    compiled.querySelectorAll<HTMLElement>('.drop-button')[0]?.click()
+    compiled.querySelectorAll<HTMLElement>('.drop-button')[0]?.click()
 
     tick()
     fixture.detectChanges()
-    expect(compiled.querySelectorAll('div.row')[5].querySelectorAll('.cell')[0]?.className).toContain('red');
-    expect(compiled.querySelectorAll('div.row')[4].querySelectorAll('.cell')[0]?.className).toContain('blue');
+    expect(compiled.querySelectorAll('div.row')[6].querySelectorAll('.cell')[0]?.className).toContain('red');
+    expect(compiled.querySelectorAll('div.row')[5].querySelectorAll('.cell')[0]?.className).toContain('blue');
   }));
 
   it('ignores invalid moves', fakeAsync(() => {
     const compiled = fixture.nativeElement as HTMLElement;
 
     for (let i = 0; i < 6; i++) {
-      compiled.querySelector('div.row')?.querySelectorAll<HTMLElement>('.cell')[0]?.click()
+      compiled.querySelectorAll<HTMLElement>('.drop-button')[0]?.click()
     }
 
     // red attempts to play in full column
-    compiled.querySelector('div.row')?.querySelectorAll<HTMLElement>('.cell')[0]?.click()
+    compiled.querySelectorAll<HTMLElement>('.drop-button')[0]?.click()
 
     // it should still be red's turn
-    compiled.querySelector('div.row')?.querySelectorAll<HTMLElement>('.cell')[1]?.click()
+    compiled.querySelectorAll<HTMLElement>('.drop-button')[1]?.click()
 
     tick()
     fixture.detectChanges()
 
-    expect(compiled.querySelectorAll('div.row')[5].querySelectorAll('.cell')[1]?.className).toContain('red');
+    expect(compiled.querySelectorAll('div.row')[6].querySelectorAll('.cell')[1]?.className).toContain('red');
   }))
 
   describe('victory conditions', () => {
@@ -77,11 +76,11 @@ describe('BoardComponent', () => {
       expect(compiled.querySelector('#red-score')?.textContent).toBe('0');
 
       for (let i = 0; i < 3; i++) {
-        compiled.querySelector('div.row')?.querySelectorAll<HTMLElement>('.cell')[i]?.click()
-        compiled.querySelector('div.row')?.querySelectorAll<HTMLElement>('.cell')[0]?.click()
+        compiled.querySelectorAll<HTMLElement>('.drop-button')[i]?.click()
+        compiled.querySelectorAll<HTMLElement>('.drop-button')[0]?.click()
       }
 
-      compiled.querySelector('div.row')?.querySelectorAll<HTMLElement>('.cell')[3]?.click()
+      compiled.querySelectorAll<HTMLElement>('.drop-button')[3]?.click()
 
       tick()
       fixture.detectChanges()
@@ -92,11 +91,11 @@ describe('BoardComponent', () => {
     it('detects vertical victory', fakeAsync(() => {
       const compiled = fixture.nativeElement as HTMLElement;
 
-      compiled.querySelector('div.row')?.querySelectorAll<HTMLElement>('.cell')[3]?.click()
-      compiled.querySelector('div.row')?.querySelectorAll<HTMLElement>('.cell')[1]?.click()
+      compiled.querySelectorAll<HTMLElement>('.drop-button')[3]?.click()
+      compiled.querySelectorAll<HTMLElement>('.drop-button')[1]?.click()
       for (let i = 0; i < 3; i++) {
-        compiled.querySelector('div.row')?.querySelectorAll<HTMLElement>('.cell')[0]?.click()
-        compiled.querySelector('div.row')?.querySelectorAll<HTMLElement>('.cell')[1]?.click()
+        compiled.querySelectorAll<HTMLElement>('.drop-button')[0]?.click()
+        compiled.querySelectorAll<HTMLElement>('.drop-button')[1]?.click()
       }
 
       tick()
@@ -108,20 +107,19 @@ describe('BoardComponent', () => {
     it('detects left diagonal victory', fakeAsync(() => {
       const compiled = fixture.nativeElement as HTMLElement;
 
-
-      compiled.querySelector('div.row')?.querySelectorAll<HTMLElement>('.cell')[0]?.click()
-      compiled.querySelector('div.row')?.querySelectorAll<HTMLElement>('.cell')[0]?.click()
-      compiled.querySelector('div.row')?.querySelectorAll<HTMLElement>('.cell')[0]?.click()
-      compiled.querySelector('div.row')?.querySelectorAll<HTMLElement>('.cell')[1]?.click()
-      compiled.querySelector('div.row')?.querySelectorAll<HTMLElement>('.cell')[1]?.click()
-      compiled.querySelector('div.row')?.querySelectorAll<HTMLElement>('.cell')[2]?.click()
+      compiled.querySelectorAll<HTMLElement>('.drop-button')[0]?.click()
+      compiled.querySelectorAll<HTMLElement>('.drop-button')[0]?.click()
+      compiled.querySelectorAll<HTMLElement>('.drop-button')[0]?.click()
+      compiled.querySelectorAll<HTMLElement>('.drop-button')[1]?.click()
+      compiled.querySelectorAll<HTMLElement>('.drop-button')[1]?.click()
+      compiled.querySelectorAll<HTMLElement>('.drop-button')[2]?.click()
 
       for (let i = 0; i < 3; i++) {
-        compiled.querySelector('div.row')?.querySelectorAll<HTMLElement>('.cell')[i]?.click()
-        compiled.querySelector('div.row')?.querySelectorAll<HTMLElement>('.cell')[4]?.click()
+        compiled.querySelectorAll<HTMLElement>('.drop-button')[i]?.click()
+        compiled.querySelectorAll<HTMLElement>('.drop-button')[4]?.click()
       }
 
-      compiled.querySelector('div.row')?.querySelectorAll<HTMLElement>('.cell')[3]?.click()
+      compiled.querySelectorAll<HTMLElement>('.drop-button')[3]?.click()
 
       tick()
       fixture.detectChanges()
@@ -133,19 +131,19 @@ describe('BoardComponent', () => {
       const compiled = fixture.nativeElement as HTMLElement;
 
 
-      compiled.querySelector('div.row')?.querySelectorAll<HTMLElement>('.cell')[1]?.click()
-      compiled.querySelector('div.row')?.querySelectorAll<HTMLElement>('.cell')[2]?.click()
-      compiled.querySelector('div.row')?.querySelectorAll<HTMLElement>('.cell')[2]?.click()
-      compiled.querySelector('div.row')?.querySelectorAll<HTMLElement>('.cell')[3]?.click()
-      compiled.querySelector('div.row')?.querySelectorAll<HTMLElement>('.cell')[3]?.click()
-      compiled.querySelector('div.row')?.querySelectorAll<HTMLElement>('.cell')[3]?.click()
+      compiled.querySelectorAll<HTMLElement>('.drop-button')[1]?.click()
+      compiled.querySelectorAll<HTMLElement>('.drop-button')[2]?.click()
+      compiled.querySelectorAll<HTMLElement>('.drop-button')[2]?.click()
+      compiled.querySelectorAll<HTMLElement>('.drop-button')[3]?.click()
+      compiled.querySelectorAll<HTMLElement>('.drop-button')[3]?.click()
+      compiled.querySelectorAll<HTMLElement>('.drop-button')[3]?.click()
 
       for (let i = 0; i < 3; i++) {
-        compiled.querySelector('div.row')?.querySelectorAll<HTMLElement>('.cell')[i]?.click()
-        compiled.querySelector('div.row')?.querySelectorAll<HTMLElement>('.cell')[4]?.click()
+        compiled.querySelectorAll<HTMLElement>('.drop-button')[i]?.click()
+        compiled.querySelectorAll<HTMLElement>('.drop-button')[4]?.click()
       }
 
-      compiled.querySelector('div.row')?.querySelectorAll<HTMLElement>('.cell')[3]?.click()
+      compiled.querySelectorAll<HTMLElement>('.drop-button')[3]?.click()
 
       tick()
       fixture.detectChanges()
@@ -156,11 +154,11 @@ describe('BoardComponent', () => {
     it('counts multiple victories', fakeAsync(() => {
       const compiled = fixture.nativeElement as HTMLElement;
 
-      compiled.querySelector('div.row')?.querySelectorAll<HTMLElement>('.cell')[3]?.click()
-      compiled.querySelector('div.row')?.querySelectorAll<HTMLElement>('.cell')[1]?.click()
+      compiled.querySelectorAll<HTMLElement>('.drop-button')[3]?.click()
+      compiled.querySelectorAll<HTMLElement>('.drop-button')[1]?.click()
       for (let i = 0; i < 4; i++) {
-        compiled.querySelector('div.row')?.querySelectorAll<HTMLElement>('.cell')[0]?.click()
-        compiled.querySelector('div.row')?.querySelectorAll<HTMLElement>('.cell')[1]?.click()
+        compiled.querySelectorAll<HTMLElement>('.drop-button')[0]?.click()
+        compiled.querySelectorAll<HTMLElement>('.drop-button')[1]?.click()
       }
 
       tick()
